3b6b3019acd5610ef0a79e959edba735c54a7f54
AnthonyZorex <119850230+AnthonyZorex@users.noreply.github.com>
2023-09-18 07:57:55 +0200

    add

---
+++ index.js
@@ -0,0 +1,14 @@
+
+/* async function connect() {
+    try {
+        let res = await fetch('http://localhost:1234/advantages', {
+            method: 'GET',
+        });
+        let data = await res.json();
+        console.log(data);
+    }
+    catch (e) {
+        console.error(e);
+    }
+}
+connect();    */
